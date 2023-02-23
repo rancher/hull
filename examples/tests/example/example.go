3e9710ecd391d1daa84f68469792f2b987efd2a5
Arvind Iyengar <arvind.iyengar@rancher.com>
2023-02-22 16:12:43 -0800

    Move test.Suite and variables to example.go

--- examples/tests/example/example.go
+++ examples/tests/example/example.go
@@ -1,7 +1,43 @@
 package example
 
 import (
+	"testing"
+
+	"github.com/aiyengar2/hull/pkg/chart"
+	"github.com/aiyengar2/hull/pkg/checker"
+	"github.com/aiyengar2/hull/pkg/test"
 	"github.com/aiyengar2/hull/pkg/utils"
 )
 
-var ChartPath = utils.MustGetPathFromModuleRoot("..", "testdata", "charts", "with-schema")
+var (
+	ChartPath = utils.MustGetPathFromModuleRoot("..", "testdata", "charts", "with-schema")
+
+	DefaultReleaseName = "with-schema"
+	DefaultNamespace   = "default"
+)
+
+var suite = test.Suite{
+	ChartPath: ChartPath,
+
+	DefaultChecks: []checker.Check{
+		{
+			Name: "Noop Default Check",
+			Func: func(*testing.T, struct{}) {},
+		},
+	},
+
+	Cases: []test.Case{
+		{
+			Name: "Using Defaults",
+
+			TemplateOptions: chart.NewTemplateOptions(DefaultReleaseName, DefaultNamespace),
+
+			Checks: []checker.Check{
+				{
+					Name: "Noop Check",
+					Func: func(*testing.T, struct{}) {},
+				},
+			},
+		},
+	},
+}
--- examples/tests/example/example_test.go
+++ examples/tests/example/example_test.go
@@ -3,41 +3,8 @@ package example
 import (
 	"testing"
 
-	"github.com/aiyengar2/hull/pkg/chart"
-	"github.com/aiyengar2/hull/pkg/checker"
 	"github.com/aiyengar2/hull/pkg/test"
 )
-
-var (
-	defaultReleaseName = "with-schema"
-	defaultNamespace   = "default"
-)
-
-var suite = test.Suite{
-	ChartPath: ChartPath,
-
-	DefaultChecks: []checker.Check{
-		{
-			Name: "Noop Default Check",
-			Func: func(*testing.T, struct{}) {},
-		},
-	},
-
-	Cases: []test.Case{
-		{
-			Name: "Using Defaults",
-
-			TemplateOptions: chart.NewTemplateOptions(defaultReleaseName, defaultNamespace),
-
-			Checks: []checker.Check{
-				{
-					Name: "Noop Check",
-					Func: func(*testing.T, struct{}) {},
-				},
-			},
-		},
-	},
-}
 
 func TestChart(t *testing.T) {
 	suite.Run(t, test.GetRancherOptions())
