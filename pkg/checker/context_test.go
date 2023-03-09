155991677d9f983311bc524207c176a20a280e1d
Arvind Iyengar <arvind.iyengar@rancher.com>
2023-03-08 16:50:33 -0800

    Add ability to extract values from a set RenderValues struct

--- pkg/checker/context_test.go
+++ pkg/checker/context_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	helmChart "helm.sh/helm/v3/pkg/chart"
 )
 
 func TestTestContext(t *testing.T) {
@@ -33,5 +34,76 @@ func TestTestContext(t *testing.T) {
 		set, found := Get[string, map[string]interface{}](tc, "nilMap")
 		assert.True(t, found)
 		assert.Nil(t, set)
+	})
+	t.Run("RenderValue When Unset", func(t *testing.T) {
+		value, found := RenderValue[string](tc, ".Chart.Name")
+		assert.False(t, found)
+		assert.Equal(t, "", value)
+	})
+	t.Run("MustRenderValue When Unset", func(t *testing.T) {
+		defer func() {
+			err := recover()
+			if err == nil {
+				assert.Fail(t, "should not have passed MustRenderValue")
+			}
+		}()
+		MustRenderValue[string](tc, ".Chart.Name")
+	})
+	tc.RenderValues = map[string]interface{}{
+		"Chart": helmChart.Metadata{
+			Name: "my-chart",
+		},
+	}
+	tc.RenderValues = map[string]interface{}{
+		"Chart": helmChart.Metadata{
+			Name: "my-chart",
+		},
+	}
+	t.Run("RenderValue When Set Struct", func(t *testing.T) {
+		value, found := RenderValue[string](tc, ".Chart.Name")
+		assert.True(t, found)
+		assert.Equal(t, "my-chart", value)
+	})
+	tc.RenderValues = map[string]interface{}{
+		"Chart": helmChart.Metadata{
+			Name: "my-chart-2",
+		},
+		"Values": map[string]interface{}{
+			"data": map[string]interface{}{
+				"hello": "world",
+			},
+		},
+	}
+	t.Run("MustRenderValue When Set Struct", func(t *testing.T) {
+		defer func() {
+			err := recover()
+			if err != nil {
+				assert.Fail(t, "should have passed MustRenderValue", err)
+			}
+		}()
+		MustRenderValue[string](tc, ".Chart.Name")
+	})
+	t.Run("RenderValue When Updated", func(t *testing.T) {
+		value, found := RenderValue[string](tc, ".Chart.Name")
+		assert.True(t, found)
+		assert.Equal(t, "my-chart-2", value)
+	})
+	t.Run("RenderValue With Set Map", func(t *testing.T) {
+		value, found := RenderValue[map[string]interface{}](tc, ".Values.data")
+		assert.True(t, found)
+		assert.Equal(t, map[string]interface{}{"hello": "world"}, value)
+	})
+	t.Run("Convert RenderValue Type To String Map", func(t *testing.T) {
+		value, found := RenderValue[map[string]string](tc, ".Values.data")
+		assert.True(t, found)
+		assert.Equal(t, map[string]string{"hello": "world"}, value)
+	})
+	t.Run("Convert RenderValue Type To Struct", func(t *testing.T) {
+		type HelloWorld struct {
+			Hello string `json:"hello"`
+		}
+		value, found := RenderValue[HelloWorld](tc, ".Values.data")
+		assert.True(t, found)
+		assert.Equal(t, HelloWorld{Hello: "world"}, value)
 	})
 }
